79fc51d355736c576bef387c08c896f01e6ba308
김현수 <hyunsoo@gimhyeonsuui-MacBookPro.local>
2026-02-23 13:25:22 +0900

    feat: robots.ts, sitemap.ts 파일 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.sooman.dev',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    // 나중에 페이지가 추가되면 여기에 더 넣을 수 있습니다.
+  ]
+}
